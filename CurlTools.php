6a51945f4ba41d3615f64c98dfcf306ddd35d2c0
北门清燕 <qq88268459@126.com>
2019-01-28 13:27:37 +0800

    update

--- CurlTools.php
+++ CurlTools.php
@@ -20,7 +20,7 @@ class CurlsTools{
     public function httpGet($url, $gzip = false, $ua='', $ck=''){
 
         $ch = curl_init($url);
-        $uas = [
+        $uas = array(
             'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
             'Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1',
@@ -32,7 +32,7 @@ class CurlsTools{
             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)',
             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)',
             'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)'
-        ];
+        );
         $c_ua = count($uas);
         if ($ua==='') {
             curl_setopt($ch,CURLOPT_USERAGENT,$uas[rand(0, $c_ua - 1)]);
